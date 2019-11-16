49626e7c48a294338313b87f65fcf6ded7205b9d
bartbutenaers <bart.butenaers@gmail.com>
2019-11-16 23:51:13 +0100

    Multiple terminal support

--- xterm_shell.js
+++ xterm_shell.js
@@ -19,25 +19,39 @@ module.exports = function(RED) {
     var os = require('os');
     var pty = require('node-pty');
     var process = require('process');
-	var path = require('path');
-	const fs = require('fs')
-
-	// Determining the path to the files in the dependent modules once.
-	// See https://discourse.nodered.org/t/use-files-from-dependent-npm-module/17978/5?u=bartbutenaers
-	var xtermPath = require.resolve("xterm");
-	xtermPath = xtermPath.substring(0, xtermPath.indexOf("xterm") + 5);
-	
-	xtermJsPath = path.join(xtermPath, 'lib', 'xterm.js');
-	if (!fs.existsSync(xtermJsPath)) {
-		console.log("Javascript file " + xtermJsPath + " does not exist");
-		xtermJsPath = null;
-	}
-	
-	xtermCssPath = path.join(xtermPath, 'css', 'xterm.css');
-	if (!fs.existsSync(xtermCssPath)) {
-		console.log("Css file " + xtermCssPath + " does not exist");
-		xtermCssPath = null;
-	}	  
+    var path = require('path');
+    const fs = require('fs');
+    
+    // -------------------------------------------------------------------------------------------------
+    // Determining the path to the files in the dependent xterm module once.
+    // See https://discourse.nodered.org/t/use-files-from-dependent-npm-module/17978/5?u=bartbutenaers
+    // -------------------------------------------------------------------------------------------------
+    var xtermPath = require.resolve("xterm");
+    xtermPath = xtermPath.substring(0, xtermPath.indexOf("xterm") + 5);
+    
+    var xtermJsPath = path.join(xtermPath, 'lib', 'xterm.js');
+    if (!fs.existsSync(xtermJsPath)) {
+        console.log("Javascript file " + xtermJsPath + " does not exist");
+        xtermJsPath = null;
+    }
+    
+    var xtermCssPath = path.join(xtermPath, 'css', 'xterm.css');
+    if (!fs.existsSync(xtermCssPath)) {
+        console.log("Css file " + xtermCssPath + " does not exist");
+        xtermCssPath = null;
+    }      
+    
+    // -------------------------------------------------------------------------------------------------
+    // Determining the path to the files in the dependent xterm-addon-fi module once.
+    // -------------------------------------------------------------------------------------------------
+    var xtermFitPath = require.resolve("xterm-addon-fit");
+    xtermFitPath = xtermFitPath.substring(0, xtermFitPath.indexOf("xterm-addon-fit") + 15);
+    
+    var xtermFitJsPath = path.join(xtermFitPath, 'lib', 'xterm-addon-fit.js');
+    if (!fs.existsSync(xtermFitJsPath)) {
+        console.log("Javascript file " + xtermFitJsPath + " does not exist");
+        xtermFitJsPath = null;
+    }
 
     function XtermShellNode(config) {
         RED.nodes.createNode(this, config);
@@ -45,13 +59,14 @@ module.exports = function(RED) {
         node.rows = config.rows;
         node.columns = config.columns;
         node.useBinaryTransport = os.platform() !== "win32";
+        node.ptyProcesses = new Map();
         
         // TODO met de options zou je node config kunnen overwriten (bv als gebruiker rows verandert en dadelijk zonder deploy resultaat wil zien)
-        node.startTerminal = function(options) {
+        node.startTerminal = function(terminalId) {
             // When the previous ptyProcess was still running, we will stop it.
-            if (node.ptyProcess) {
+            if (node.ptyProcesses.has(terminalId)) {
                 console.log("Previous ptyProcess was already started");
-                node.stopTerminal();
+                node.stopTerminal(terminalId);
             }
             
             try {
@@ -61,7 +76,7 @@ module.exports = function(RED) {
                 var rows = parseInt(node.rows);
                 var cols = parseInt(node.columns);
                 
-                node.ptyProcess = pty.spawn(process.platform === 'win32' ? 'cmd.exe' : 'bash', [], {
+                var ptyProcess = pty.spawn(process.platform === 'win32' ? 'cmd.exe' : 'bash', [], {
                   name: 'xterm-256color',
                   cols: cols || 80,
                   rows: rows || 24,
@@ -69,21 +84,22 @@ module.exports = function(RED) {
                   env: env,
                   encoding: node.useBinaryTransport ? null : 'utf8'
                 });
+                
+                node.ptyProcesses.set(terminalId, ptyProcess);
             
                 //TODO Send PID to show in config screen ???????? res.send(term.pid.toString());
         
-                node.ptyProcess.on('data', function(data) {
+                ptyProcess.on('data', function(data) {
                     try {
                         console.log("data send via websocket to client");
                         //TODO send data in output message ???? node.send(data);
                         
                         // Convert the terminal data to base64, before sending it to the client
-						var buff = new Buffer(data);
-						var base64Data = buff.toString('base64');
-
-                        RED.comms.publish("xterm_shell", { id: node.id, data: base64Data });
+                        var buff = new Buffer(data);
+                        var base64Data = buff.toString('base64');
+                        RED.comms.publish("xterm_shell", JSON.stringify( { nodeId: node.id, terminalId: terminalId, data: base64Data } ));
                     } 
-					catch (ex) {
+                    catch (ex) {
                         // The WebSocket is not open, ignore
                     }
                 });
@@ -96,10 +112,10 @@ module.exports = function(RED) {
             }
         }
         
-        node.writeDataToTerminal = function(data) {
+        node.writeDataToTerminal = function(terminalId, data) {
             try {
-                node.ptyProcess.write(data);
-                
+                var ptyProcess = node.ptyProcesses.get(terminalId);
+                ptyProcess.write(data);
             }
             catch (err) {
                 node.error(err);
@@ -107,15 +123,16 @@ module.exports = function(RED) {
             }  
         }
         
-        node.stopTerminal = function() {      
-            if (!node.ptyProcess) {
+        node.stopTerminal = function(terminalId) {  
+            var ptyProcess = node.ptyProcesses.get(terminalId);
+            
+            if (!ptyProcess) {
                 return;
             }
             
             try {
-                // TODO een iets elegantere manier zoeken ...
-                node.ptyProcess.kill();
-                node.ptyProcess = null;
+                ptyProcess.kill();
+                node.ptyProcesses.delete(terminalId);
                 
                 console.log("Pty process stopped");
             }
@@ -131,8 +148,14 @@ module.exports = function(RED) {
 
         node.on("close", function() {
             //TODO socket.emit('data', '\r\n*** SSH CONNECTION CLOSED ***\r\n');
-
-            node.stopTerminal({});
+            
+            var terminalIds = Array.from(node.ptyProcesses.keys());
+            
+            // Stop all the spawned pty processes
+            for (var i = 0; i < terminalIds.length; i++) {
+                var terminalId = terminalIds[i];
+                node.stopTerminal(terminalId);
+            }
             
             if (done) {
                 done();
@@ -143,49 +166,65 @@ module.exports = function(RED) {
     RED.nodes.registerType("xterm_shell", XtermShellNode);
     
     // Process the requests from the flow editor
-    RED.httpAdmin.get('/xterm_shell/:config_node_id/:command/:info', function(req, res) {
+    RED.httpAdmin.get('/xterm_shell/:node_id/:terminal_id/:command/:info', function(req, res) {
         var xtermShellNode;
         
         if (req.params.command !== "js" && req.params.command !== "css") {
             // There might be N different xterm shell nodes active, each with their own terminal.  So get the right one ...
-            xtermShellNode = RED.nodes.getNode(req.params.config_node_id);
-			
-			if (!xtermShellNode) {
-				res.status(404).json('Cannot find node with id = ' + req.params.config_node_id);
-			}
+            xtermShellNode = RED.nodes.getNode(req.params.node_id);
+            
+            if (!xtermShellNode) {
+                res.status(404).json('Cannot find node with id = ' + req.params.node_id);
+            }
         }     
-	 
+
         switch (req.params.command) {
             case "js":
-                if (xtermJsPath) {
-					// Send the requested .js file to the client (info contains .js file name)
-					res.sendFile(xtermJsPath);
-				}
-				else {
-					res.status(404).json('Javascript file does not exist');
-				}
+                switch (req.params.info) {
+                    case "xterm.js":
+                        if (xtermJsPath) {
+                            // Send the requested .js file to the client (info contains .js file name)
+                            res.sendFile(xtermJsPath);
+                        }
+                        else {
+                            res.status(404).json('Javascript terminal library is not available');
+                        }
+                        break;
+                    case "xterm-addon-fit.js":
+                        if (xtermFitJsPath) {
+                            // Send the requested .js file to the client (info contains .js file name)
+                            res.sendFile(xtermFitJsPath);
+                        }
+                        else {
+                            res.status(404).json('Javascript terminal fit addon is not available');
+                        }
+                        break;    
+                    default:
+                        console.log("Unknown javascript file '" + req.params.info + "'");
+                        res.status(404).json('Unknown javascript file');                        
+                }
                 break;
             case "css":
                 if (xtermCssPath) {
-					// Send the requested .css file to the client (info contains .css file name)
-					res.sendFile(xtermCssPath);
-				}
-				else {
-					res.status(404).json('Css file does not exist');
-				}
+                    // Send the requested .css file to the client (info contains .css file name)
+                    res.sendFile(xtermCssPath);
+                }
+                else {
+                    res.status(404).json('Css file does not exist');
+                }
                 break;
             case "start":
-                xtermShellNode.startTerminal({});
+                xtermShellNode.startTerminal(req.params.terminal_id);
                 res.status(200).json('success');
                 break;
             case "stop":
-                xtermShellNode.stopTerminal();
+                xtermShellNode.stopTerminal(req.params.terminal_id);
                 res.status(200).json('success');
                 break;
             case "write":        
-				var base64Decoded = new Buffer(req.params.info, 'base64').toString('ascii');
+                var base64Decoded = new Buffer(req.params.info, 'base64').toString('ascii');
                 // Process the command line data (info contains command line input)
-                xtermShellNode.writeDataToTerminal(base64Decoded);
+                xtermShellNode.writeDataToTerminal(req.params.terminal_id, base64Decoded);
                 res.status(200).json('success');
                 break;
             default:
