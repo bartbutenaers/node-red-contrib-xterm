9226c67b86d788ace31226f4628b4e732f3be23b
bartbutenaers <bart.butenaers@gmail.com>
2020-01-12 10:55:17 +0100

    Use POST

--- xterm_config.js
+++ xterm_config.js
@@ -264,17 +264,22 @@
         }
     });
 
-    // Process the requests from the flow editor
-    RED.httpAdmin.get('/xterm_shell/:terminal_id/:command/:info', RED.auth.needsPermission('xterm_shell.write'), function(req, res) {
+    // Process the POST requests from the flow editor
+    RED.httpAdmin.post('/xterm_shell/command', RED.auth.needsPermission('xterm_shell.write'), function(req, res) {
         // When a config node is available on the server side, we see whether logging should be enabled
         var loggingEnabled = xtermShellNode && xtermShellNode.loggingEnabled;
 
+        // Get the POST parameters
+        var command     = req.body.command;
+        var terminal_id = req.body.terminal_id;
+        var info        = req.body.info;
+        
         try {
-            switch (req.params.command) {
+            switch (command) {
                 case "start":
                     // The request (info) will contain the default dimensions '<default_rows>;<default_columns>'.
                     // This way the default dimensions only need to be hardcoded on the client side...
-                    var params = req.params.info.split(";");
+                    var params = info.split(";");
                     var rows = params[0];
                     var columns = params[1];
                     
@@ -284,21 +289,21 @@
                         columns = xtermShellNode.columns;
                     }
 
-                    startTerminal(req.params.terminal_id, rows, columns, loggingEnabled);
+                    startTerminal(terminal_id, rows, columns, loggingEnabled);
 
                     // Pass the rows and columns to the client, which have been used on the server to start the pseudo terminal.
                     // The client needs to use the SAME values to create an Xterm.js terminal window, to avoid conflicts!
                     res.status(200).json({rows: rows, columns: columns});
                     break;
                 case "stop":
-                    stopTerminal(req.params.terminal_id, "stop button", loggingEnabled);
+                    stopTerminal(terminal_id, "stop button", loggingEnabled);
                     res.status(200).json({});
                     break;
                 case "write":
-                    var base64Decoded = new Buffer(req.params.info, 'base64').toString('ascii');
+                    var base64Decoded = new Buffer(info, 'base64').toString('ascii');
                     
                     // Process the command line data (info contains command line input)
-                    var errorText = writeDataToTerminal(req.params.terminal_id, base64Decoded, loggingEnabled);
+                    var errorText = writeDataToTerminal(terminal_id, base64Decoded, loggingEnabled);
                     
                     // The xterm_in node (doesn't listen to the websocket so it) has to be informed whether the command
                     // has been transferred to the pseudo terminal correctly
@@ -311,11 +316,11 @@
                     break;
                 case "heartbeat":
                     // Restart the timer whenever a heartbeat arrives
-                    startTimer(req.params.terminal_id);
+                    startTimer(terminal_id);
                     res.status(200).json({}); 
                     break;
                 default:
-                    console.log("Unknown command '" + req.params.command + "'");
+                    console.log("Unknown command '" + command + "'");
                     res.status(404).json({error: 'Unknown command'});
             }
         }
@@ -323,7 +328,7 @@
             // Lots of server-side processing, so lots of things can go wrong.
             // Avoid that the client doesn't know what is going on,i.e. avoid internal server errors (status 500)
             res.status(500).json({error: err.name, message: err.message, stack: err.stack });
-            console.log("Error while executing command '" + req.params.command + "':");
+            console.log("Error while executing command '" + command + "':");
             console.log(err.stack);
         }
     });
